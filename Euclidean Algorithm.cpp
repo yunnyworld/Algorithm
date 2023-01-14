f916dde46cb7fa14c9526deb0a986b5a168c08ea
yunnyworld <50856004+yunnyworld@users.noreply.github.com>
2023-01-14 17:09:52 +0900

    Update Euclidean Algorithm.cpp

--- Euclidean Algorithm.cpp
+++ Euclidean Algorithm.cpp
@@ -2,12 +2,5 @@
 #define ll long long int
 using namespace std;
 
-ll gcd(ll a, ll b)
-{
-    return b ? gcd(b, a % b) : a; 
-}
-
-ll lcm(ll a, ll b)
-{
-    return a / gcd(a, b) * b;
-}
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
