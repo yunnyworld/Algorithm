9e8f20b589f0f5f42a44649585b87445a52b17d6
yunnyworld <50856004+yunnyworld@users.noreply.github.com>
2023-01-14 17:10:31 +0900

    Update MyPSTemplate.cpp

--- MyPSTemplate.cpp
+++ MyPSTemplate.cpp
@@ -1,35 +1,28 @@
 #include <bits/stdc++.h>
 #define ll long long int
 #define pll pair<ll, ll>
-#define pdd pair<double, double>
 #define X first
 #define Y second
 using namespace std;
-ll gcd(ll a, ll b)
-{
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
-#define lcm(a, b) a * b / gcd(a, b)
-const int INF = 987654321;
-const int MOD = 1e9 + 7;
+const ll INF = 987654321;
+const ll MOD = 1e9 + 7;
 int dx[] = { -1, 1, 0, 0 };
 int dy[] = { 0, 0, -1, 1 };
-
-
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
 
 void solve()
 {
-
+	
 }
 
 int main()
 {
-    ios::sync_with_stdio(0); cin.tie(0);
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
+	freopen("input.txt", "r", stdin);
 #endif
-    int tc = 1; cin >> tc;
-    while (tc--) solve();
-    return 0;
+	int tc = 1; //cin >> tc;
+	while (tc--) solve();
+	return 0;
 }
