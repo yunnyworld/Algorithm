a004ba584a7b9e07a78cedafbb09f4944ae05a07
yunnyworld <50856004+yunnyworld@users.noreply.github.com>
2023-01-15 20:08:10 +0900

    Create FlT.cpp

---
+++ FlT.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+/*
+Fermat's little Theorem (FlT, 페르마 소정리)
+
+페르마 소정리: p가 소수이고 a, p가 서로소일 때 a^(p-1) = 1 (mod p)
+- a^(p-1) = 1 (mod p) 
+- a * a^(p-2) = 1 (mod p) : a^(p-2)는 a의 곱셈 역원이다
+*/
+
+// 보통 분할 정복을 이용한 거듭제곱을 이용해 구현한다.
+ll pow(ll a, ll n)
+{
+    if(n==0) return 1ll;
+    ll ret=pow(a, n/2);
+    ret*=ret;
+    if(n%2) ret*=a;
+    return ret;
+}
