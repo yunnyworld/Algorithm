ff4b41c467983935bd16eacf65a388d24254aa9f
yunnyworld <50856004+yunnyworld@users.noreply.github.com>
2023-01-17 06:26:25 +0900

    Update EulerPhi.cpp

--- EulerPhi.cpp
+++ EulerPhi.cpp
@@ -5,7 +5,8 @@ using namespace std;
 Euler Phi function (오일러 피 함수)
 - E(x) = x이하의 수 중 x와 서로소인 수의 개수
 - E(p) = p-1 (p는 소수)
-- E(p^n) = (p-1)*p^(n-1) - ㄱ
+- E(p^n) = p^n - p^(n-1) (by definition)
+         = (p-1)*p^(n-1) - ㄱ
 - E(p*q) = E(p)*E(q) (p와 q가 서로소일때)
 - m = p1^n1 * p2^n2 * ... * pk^nk 일때, ㄱ에 의해
   E(m) = m*((p1-1)/p1)*((p2-1)/p2)*...*((pk-1)/pk) 이다.
