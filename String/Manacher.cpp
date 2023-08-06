cc15f85a93dc91fe117a61f612fcceaf6e72635d
Hoeyun Jung <yunnyworld@gmail.com>
2023-08-06 12:46:06 +0900

    Update Manacher.cpp

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,34 +1,38 @@
-#include <bits/stdc++>
+#include <bits/stdc++.h>
+#define ll long long int
 using namespace std;
 /*
 Manacher's Algorithm
-Reference: 
-https://www.crocus.co.kr/1075
+Reference:
 https://nicotina04.tistory.com/250
 
 O(n)
 */
-string s; //주어진 문자열
-string ss; //전처리 된 문자열
-ll ma[4000005]; //ma[i] = i가 중심인 펠린드롬의 길이
-ll r=-1, c=-1;
-
-void manacher()
+struct manacher
 {
-    for(char cc : s)
+    string s; // input string
+    string ss = ""; // preprocessed string
+    vector<ll> ma; // ma[i] := length of palindrome whose center is i
+    ll r = -1, c = -1; // center of previous palindrome, radius of previous palindrome
+    manacher(string& s) : s(s), ma(s.size() * 2 + 2, 0) {}
+    void go()
     {
+        for (char cc : s)
+        {
+            ss.push_back('#'); // dummy character for even length palindrome
+            ss.push_back(cc);
+        }
         ss.push_back('#');
-        ss.push_back(cc);
+        for (ll i = 0; i < ss.size(); i++)
+        {
+            if (i <= r) ma[i] = min(r - i, ma[2 * c - i]); // get ma[i]
+            while (i + ma[i] + 1 < ss.size() && i - ma[i] - 1 >= 0 && ss[i + ma[i] + 1] == ss[i - ma[i] - 1])
+                ma[i]++; // update ma[i]
+            if (i + ma[i] > r) // update c and r
+            {
+                c = i; 
+                r = i + ma[i];
+            }
+        }
     }
-    ss.push_back('#');
-
-    for(ll i=0;i<ss.size();i++)
-    {
-        if(i<=r) ma[i]=min(r-i, ma[2*c-i]);
-
-        while(i+ma[i]+1<ss.size() && i-ma[i]-1>=0 && ss[i+ma[i]+1]==ss[i-ma[i]-1])
-            ma[i]++;
-        
-        if(i+ma[i]>r) {c=i; r=i+ma[i];}
-    }
-}
+};
