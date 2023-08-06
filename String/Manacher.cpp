6432827ea67e424d4d70e6060605786d6e83e0ce
Hoeyun Jung <yunnyworld@gmail.com>
2023-08-06 13:20:47 +0900

    Update Manacher.cpp

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -5,14 +5,15 @@ using namespace std;
 Manacher's Algorithm
 Reference:
 https://nicotina04.tistory.com/250
-
+Time Complexity Analysis:
+https://www.youtube.com/watch?v=OLpy_Mh6NZY
 O(n)
 */
 struct manacher
 {
     string s; // input string
     string ss = ""; // preprocessed string
-    vector<ll> ma; // ma[i] := length of palindrome whose center is i
+    vector<ll> ma; // ma[i] := length of palindrome which center is i
     ll r = -1, c = -1; // center of previous palindrome, radius of previous palindrome
     manacher(string& s) : s(s), ma(s.size() * 2 + 2, 0) {}
     void go()
@@ -30,7 +31,7 @@ struct manacher
                 ma[i]++; // update ma[i]
             if (i + ma[i] > r) // update c and r
             {
-                c = i; 
+                c = i;
                 r = i + ma[i];
             }
         }
